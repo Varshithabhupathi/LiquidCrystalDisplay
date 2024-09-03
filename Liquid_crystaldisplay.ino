05423c8ba011783d28555000144463922c8dc847
Varshitha_varshi <varshithabhupathi@gmail.com>
2024-09-03 10:29:33 +0530

    Add files via upload

---
+++ Liquid_crystaldisplay.ino
@@ -0,0 +1,21 @@
+# include <LiquidCrystal.h>
+
+LiquidCrystal lcd(2,3,4,5,6,7);
+
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.setCursor(5,0);
+  lcd.print("WELCOME");
+  lcd.setCursor(0,1);
+  lcd.print("VARSHITHA");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print("EMBEDDED SDC");
+  lcd.setCursor(5,1);
+  lcd.print("ARDUINO UNO");
+}
+void loop() {
+  
+}
